e68c18a37a0194f349ae577761c2647793c43d90
Agathe BOSCH <agbosch@e2r3p2.42.fr>
2020-09-20 17:35:45 +0200

    feah

--- fill_rownew.c
+++ fill_rownew.c
@@ -6,7 +6,7 @@
 /*   By: spoliart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/19 18:10:16 by spoliart          #+#    #+#             */
-/*   Updated: 2020/09/20 17:14:01 by agbosch          ###   ########.fr       */
+/*   Updated: 2020/09/20 17:35:37 by agbosch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,10 @@ void	ft_print(char **tab)
 	{
 		j = -1;
 		while (++j < 4)
+		{
 			write(1, &tab[i][j], 1);
+			write(1, " ", 1);
+		}
 		write(1, "\n", 1);
 	}
 	write(1, "\n", 1);
@@ -43,10 +46,10 @@ char	fill_row2(char **board, int row, char **tab_arg)
 		return (tab_is_ok(board, tab_arg));
 	eureka = 0;
 	board[row][0] = '0';
-	while (++board[row][0] <= '4' && !eureka)
+	while (board[row][0]++ <= '3' && !eureka)
 	{
 		board[row][1] = '0';
-		while (++board[row][1] <= '4' && !eureka)
+		while (board[row][1]++ <= '3' && !eureka)
 		{
 			if(board[row][1] == board[row][0]) 
 				continue ;
@@ -62,12 +65,12 @@ char	fill_row2_bis(char **board, int row, char **tab_arg)
 
 	eureka = 0;
 	board[row][2] = '0';
-	while (++board[row][2] <= '4' && !eureka)
+	while (board[row][2]++ <= '3' && !eureka)
 	{
 		if(board[row][2] == board[row][0] || board[row][2] == board[row][1]) 
 			continue ;
 		board[row][3] = '0';
-		while (++board[row][3] <= '4' && !eureka)
+		while (board[row][3]++ <= '3' && !eureka)
 		{
 			if(board[row][3] == board[row][0] || board[row][3] == board[row][1]
 				|| board[row][3] == board[row][2]) 
@@ -84,11 +87,8 @@ int		row_ok(char **board, int row)
 	int i;
 	int col;
 
-	i = -1;
 	col = -1;
-	if (row == 0)
-		return (1);
-	while (++col <= 4)
+	while (++col < 4)
 		i = -1;
 		while (++i < row)
 			if (board[i][col] == board[row][col])
